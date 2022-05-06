784184cd3899a734a44088506e6a9c63138ddbd0
Joel Stevick <joelstevick@outlook.com>
2022-05-06 19:48:28 -0400

    refactored -> post()

--- OnTheMap/Services/UdacityApi.swift
+++ OnTheMap/Services/UdacityApi.swift
@@ -22,28 +22,25 @@ class UdacityApi {
         
     }
     
-    private func buildRequest<T: Encodable>(url: String, method: String, body: T) throws -> URLRequest {
+    private func buildPostRequest<T: Encodable>(url: String, body: T) throws -> URLRequest {
         var request = URLRequest(url: URL(string: url)!)
-        request.httpMethod = method
+        request.httpMethod = "POST"
         
-        if method.uppercased() == "POST" {
-            let encoder = JSONEncoder()
-            
-            request.addValue("application/json", forHTTPHeaderField: "Accept")
-            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            
-            request.httpBody = try encoder.encode(body)
-            
-        }
+        let encoder = JSONEncoder()
+        
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        request.httpBody = try encoder.encode(body)
         
         return request
         
     }
-    private func sendRequest< T: Encodable, ResponseType: Decodable>
-    (url: UdacityUrl,  method: String, body: T, responseType: ResponseType.Type) async ->
+    private func post< T: Encodable, ResponseType: Decodable>
+    (url: UdacityUrl, body: T, responseType: ResponseType.Type) async ->
     Result<ResponseType, UdacityApiError> {
         do {
-            let request = try buildRequest(url: url.rawValue, method: method, body: body)
+            let request = try buildPostRequest(url: url.rawValue, body: body)
             
             // send the request over the wire
             let session = URLSession.shared
@@ -66,7 +63,7 @@ class UdacityApi {
     func signin(username: String, password: String) async -> String? {
         let body = SignInRequest(username: username, password: password)
         
-        let result = await sendRequest(url: UdacityUrl.session, method: "POST", body: body, responseType: SignInResponse.self)
+        let result = await post(url: UdacityUrl.session, body: body, responseType: SignInResponse.self)
         
         switch result {
         case .success(let response):
