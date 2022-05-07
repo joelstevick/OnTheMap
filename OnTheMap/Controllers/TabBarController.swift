b5ef1f75afafcf0bb606129d49e5305320f9cb43
Joel Stevick <joelstevick@outlook.com>
2022-05-07 19:53:46 -0400

    remove back button

---
+++ OnTheMap/Controllers/TabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  TabBarController.swift
+//  OnTheMap
+//
+//  Created by joel.stevick on 5/7/22.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.hidesBackButton = true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
