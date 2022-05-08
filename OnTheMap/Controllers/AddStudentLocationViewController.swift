5d085ee75ddd383132763c2f72521696d1b94064
Joel Stevick <joelstevick@outlook.com>
2022-05-08 14:26:03 -0400

    add student location

---
+++ OnTheMap/Controllers/AddStudentLocationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddStudentLocationViewController.swift
+//  OnTheMap
+//
+//  Created by joel.stevick on 5/8/22.
+//
+
+import UIKit
+
+class AddStudentLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
