8af1c067fa99250a8cc9800750030b2485a40f6d
Joel Stevick <joelstevick@outlook.com>
2022-05-09 20:14:52 -0400

    add cancel button

--- OnTheMap/Controllers/AddStudentLocationViewController.swift
+++ OnTheMap/Controllers/AddStudentLocationViewController.swift
@@ -15,8 +15,11 @@ enum Step: String {
 
 import UIKit
 
-class AddStudentLocationViewController: UIViewController {
+class AddStudentLocationViewController: UIViewController, UITextFieldDelegate {
 
+    // MARK: - Properties
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,14 +29,6 @@ class AddStudentLocationViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
