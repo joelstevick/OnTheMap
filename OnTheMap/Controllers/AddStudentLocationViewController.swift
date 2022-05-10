9c349c75e410905074b657bd4b98f25520917be9
Joel Stevick <joelstevick@outlook.com>
2022-05-09 20:37:31 -0400

    continue disabled if no text:

--- OnTheMap/Controllers/AddStudentLocationViewController.swift
+++ OnTheMap/Controllers/AddStudentLocationViewController.swift
@@ -16,18 +16,31 @@ enum Step: String {
 import UIKit
 
 class AddStudentLocationViewController: UIViewController, UITextFieldDelegate {
-
+    
     // MARK: - Properties
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var continueBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // initialize state
+        
+        textField.delegate = self
+        
+        continueBtn.isEnabled = false
+        
+        // initialize step-state
         State.shared.reset()
         
     }
     
+    // MARK: - Textfield delegate
+    @IBAction func textViewDidChange(_ textField: UITextField) {
+        if let text = textField.text {
+            continueBtn.isEnabled = text.trimmingCharacters(in: CharacterSet.whitespaces).count > 0
+        } else {
+            continueBtn.isEnabled = false
+        }
+    }
     // MARK: - Actions
     @IBAction func cancelBtnPressed(_ sender: Any) {
         
