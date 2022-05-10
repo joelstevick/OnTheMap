b1a4d0098a82e97ac093e8580cbc58a11328c39e
Joel Stevick <joelstevick@outlook.com>
2022-05-10 06:11:11 -0400

    My Link

---
+++ OnTheMap/Controllers/MyLinkViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MyLinkViewController.swift
+//  OnTheMap
+//
+//  Created by joel.stevick on 5/10/22.
+//
+
+import UIKit
+
+class MyLinkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
