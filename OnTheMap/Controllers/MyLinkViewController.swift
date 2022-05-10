baaf043380cec6113119473352e04f5bae0eb686
Joel Stevick <joelstevick@outlook.com>
2022-05-10 06:14:45 -0400

    back btn pressed

--- OnTheMap/Controllers/MyLinkViewController.swift
+++ OnTheMap/Controllers/MyLinkViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class MyLinkViewController: UIViewController {
 
+    // MARK: - Properties
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    // MARK: - Actions
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismiss(animated: true)
+    }
 }
