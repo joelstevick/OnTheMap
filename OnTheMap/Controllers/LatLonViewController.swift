ffd94a1fa2e2d5fd224e1522067e6d590dbde8ad
Joel Stevick <joelstevick@outlook.com>
2022-05-10 10:38:16 -0400

    refactored class name

--- OnTheMap/Controllers/LatLonViewController.swift
+++ OnTheMap/Controllers/LatLonViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import CoreLocation
 import MapKit
 
-class CollectLatLonViewController: UIViewController {
+class LatLonViewController: UIViewController {
 
     // MARK: - Properties
     
