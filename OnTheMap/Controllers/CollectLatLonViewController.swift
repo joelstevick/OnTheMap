3a24dbd6bd683d025e5146607c5a8042fc014607
Joel Stevick <joelstevick@outlook.com>
2022-05-10 04:50:01 -0400

    add view for collecting lat-lon

---
+++ OnTheMap/Controllers/CollectLatLonViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CollectLatLonViewController.swift
+//  OnTheMap
+//
+//  Created by joel.stevick on 5/10/22.
+//
+
+import UIKit
+
+class CollectLatLonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+}
