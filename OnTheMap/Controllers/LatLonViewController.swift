75fd0be47789cc7226610b53ca77b670a753e04d
joel stevick <joelstevick@outlook.com>
2022-05-14 06:19:13 -0400

    zoom to location

--- OnTheMap/Controllers/LatLonViewController.swift
+++ OnTheMap/Controllers/LatLonViewController.swift
@@ -10,20 +10,25 @@ import CoreLocation
 import MapKit
 
 class LatLonViewController: UIViewController {
-
+    
     // MARK: - Properties
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // get coordinates from previous step
         let coordinate = State.shared.getState(key: StateKey.coordinate.rawValue) as! CLLocationCoordinate2D
         
         let annotation = MKPointAnnotation()
         annotation.coordinate = coordinate
-      
+        
         mapView.addAnnotation(annotation)
+        
+        // Zoom to location
+        let viewRegion = MKCoordinateRegion(center: coordinate, latitudinalMeters: 200, longitudinalMeters: 200)
+        mapView.setRegion(viewRegion, animated: true)
+        mapView.showsUserLocation = true
     }
     
     // MARK: - Actions
@@ -36,13 +41,13 @@ class LatLonViewController: UIViewController {
                 presentingViewController2.dismiss(animated: true)
             }
         }
-       
+        
     }
     @IBAction func backBtnPressed(_ sender: Any) {
         dismiss(animated: true)
     }
     @IBAction func continueBtnPressed(_ sender: Any) {
-
+        
         
         // next step
         self.performSegue(withIdentifier: "CollectMediaURL", sender: self)
