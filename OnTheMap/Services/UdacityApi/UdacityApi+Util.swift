1df572554c932be2309c7c251620a90cf64d3885
joel stevick <joelstevick@outlook.com>
2022-05-14 04:46:04 -0400

    code cleanup

--- OnTheMap/Services/UdacityApi/UdacityApi+Util.swift
+++ OnTheMap/Services/UdacityApi/UdacityApi+Util.swift
@@ -22,40 +22,4 @@ extension UdacityApi {
             showError(viewController: viewController, message: "Could not get the name")
         }
     }
-    
-    func canonicalize(_ studentLocations: [StudentLocation]?) -> [StudentLocation]? {
-        if var sl = studentLocations {
-            // remove junk rows
-            sl = sl.filter({ studentLocation in
-                if let _ = URL(string: studentLocation.mediaURL) {
-                    return studentLocation.firstName.count > 0
-                } else {
-                    return false
-                }
-            })
-            
-            // uniqueness
-            var unique = [String: StudentLocation]()
-            
-            sl.forEach({ studentLocation in
-                unique[studentLocation.uniqueKey!] = studentLocation
-            })
-            
-            sl = []
-            
-            for (_, studentLocation) in unique {
-                sl.append(studentLocation)
-            }
-            
-            
-            // sort in reverse chrono
-            sl.sort { stringToDate(isoDate: $0.updatedAt) > stringToDate(isoDate: $1.updatedAt)  }
-            
-            return sl
-        } else {
-            return nil
-        }
-        
-    }
-    
 }
---
+++ OnTheMap/Utils/canonicalize.swift
@@ -0,0 +1,43 @@
+//
+//  canonicalize.swift
+//  OnTheMap
+//
+//  Created by Joel Stevick on 5/14/22.
+//
+
+import Foundation
+
+func canonicalize(_ studentLocations: [StudentLocation]?) -> [StudentLocation]? {
+    if var sl = studentLocations {
+        // remove junk rows
+        sl = sl.filter({ studentLocation in
+            if let _ = URL(string: studentLocation.mediaURL) {
+                return studentLocation.firstName.count > 0
+            } else {
+                return false
+            }
+        })
+        
+        // uniqueness
+        var unique = [String: StudentLocation]()
+        
+        sl.forEach({ studentLocation in
+            unique[studentLocation.uniqueKey!] = studentLocation
+        })
+        
+        sl = []
+        
+        for (_, studentLocation) in unique {
+            sl.append(studentLocation)
+        }
+        
+        
+        // sort in reverse chrono
+        sl.sort { stringToDate(isoDate: $0.updatedAt) > stringToDate(isoDate: $1.updatedAt)  }
+        
+        return sl
+    } else {
+        return nil
+    }
+    
+}
