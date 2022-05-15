8e53d031705675eb2a814c8312a6edc925cb00d3
joel stevick <joelstevick@outlook.com>
2022-05-15 17:13:06 -0400

    stop animating if geocoder fails

--- OnTheMap/Controllers/AddStudentLocationViewController.swift
+++ OnTheMap/Controllers/AddStudentLocationViewController.swift
@@ -78,34 +78,31 @@ class AddStudentLocationViewController: UIViewController, UITextFieldDelegate {
     @IBAction func continueBtnPressed(_ sender: Any) {
         activityIndicatorView.startAnimating()
         
-        do {
-            // convert mapString to coords
-            let mapString = textField.text!.trimmingCharacters(in: CharacterSet.whitespaces)
-            
-            let geoCoder = CLGeocoder()
-            geoCoder.geocodeAddressString(mapString) { (placemarks, error) in
-                guard
-                    let placemarks = placemarks,
-                    let location = placemarks.first?.location
-                else {
-                    // handle no location found
-                    self.unrecognizedLocation.isHidden = false
-                    return
-                }
-                
-                // persist state
-                State.shared.setState(key: StateKey.mapString.rawValue, value: mapString)
-                State.shared.setState(key: StateKey.coordinate.rawValue, value: location.coordinate)
-                
-                self.activityIndicatorView.stopAnimating();
-                
-                // next step
-                self.performSegue(withIdentifier: "CollectLatLon", sender: self)
+        // convert mapString to coords
+        let mapString = textField.text!.trimmingCharacters(in: CharacterSet.whitespaces)
+        
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(mapString) { (placemarks, error) in
+            guard
+                let placemarks = placemarks,
+                let location = placemarks.first?.location
+            else {
+                // handle no location found
+                self.unrecognizedLocation.isHidden = false
+                self.activityIndicatorView.stopAnimating()
+                return
             }
             
-        } catch {
-            activityIndicatorView.stopAnimating()
+            // persist state
+            State.shared.setState(key: StateKey.mapString.rawValue, value: mapString)
+            State.shared.setState(key: StateKey.coordinate.rawValue, value: location.coordinate)
+            
+            self.activityIndicatorView.stopAnimating();
+            
+            // next step
+            self.performSegue(withIdentifier: "CollectLatLon", sender: self)
         }
+        
     }
     @IBAction func cancelBtnPressed(_ sender: Any) {
         
